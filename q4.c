73aaf249317e3edad34a6f2b2a3d51b46d8c4b49
Aditya0991 <92169367+Aditya0991@users.noreply.github.com>
2021-10-10 22:50:32 +0530

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    float a=3.2,b=5.1;
+    printf("multipuly a*b:%f",a*b);
+    return 0;
+}
---
+++ q6.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a=12,b=4;
+    printf("quotient:%d \n",a/b);
+    printf("remainder:%d",a%b);
+    return 0;
+}
